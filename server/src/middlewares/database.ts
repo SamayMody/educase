5f401fd0c302b5943724497c7246b7ae333e01ce
Srmsamay <sm7525@srmist.edu.in>
2025-04-23 19:34:11 +0530

    Changes for deployment

--- server/src/middlewares/database.ts
+++ server/src/middlewares/database.ts
@@ -2,13 +2,25 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// export const connection = () => {
+//     const pool = mysql.createPool({
+//         host: process.env.MYSQL_HOST,
+//         user: process.env.MYSQL_USER,
+//         password: process.env.MYSQL_PASSWORD,
+//         database: process.env.MYSQL_DATABASE,
+//         port: 55825,
+//     }).promise()
+//     return pool;
+
+// };
+
 export const connection = () => {
     const pool = mysql.createPool({
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE,
-        port: 6872,
+        port: Number(process.env.MYSQL_PORT),
     }).promise()
     return pool;
 
